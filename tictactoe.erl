29376444d592b17aeeda85faabea654efba28150
GabrielNicolasAvellaneda <avellaneda.gabriel@gmail.com>
2015-11-18 13:46:20 -0300

    Check board for a victory
    Signed-off-by: GabrielNicolasAvellaneda <avellaneda.gabriel@gmail.com>

--- tictactoe.erl
+++ tictactoe.erl
@@ -24,6 +24,67 @@ create_empty_board() ->
 	{empty, empty, empty,
 	 empty, empty, empty,
 	 empty, empty, empty}.
+
+-spec check_board(tuple()) -> {victory, x} | {victory, o} | draw.
+check_board(Board) ->
+	case Board of
+
+		{Mark, Mark, Mark,
+		 _, _, _,
+		 _, _, _} -> {victory, Mark};
+
+		{_, _, _,
+		 Mark, Mark, Mark,
+		 _, _, _} -> {victory, Mark};
+
+		{_, _, _,
+		 _, _, _,
+		 Mark, Mark, Mark} -> {victory, Mark};
+
+		{Mark, _, _,
+		 Mark, _, _,
+		 Mark, _, _} -> {victory, Mark};
+
+		{_, Mark, _,
+		 _, Mark, _,
+		 _, Mark, _} -> {victory, Mark};
+
+		{_, _, Mark,
+		 _, _, Mark,
+		 _, _, Mark} -> {victory, Mark};
+
+		{Mark, _, _,
+		 _, Mark, _,
+		 _, _, Mark} -> {victory, Mark};
+
+		{_, _, Mark,
+		 _, Mark, _,
+		 Mark, _, _} -> {victory, Mark}
+	end.
 	
-create_emtpy_board_test() ->
+create_empty_board_test() ->
 	?assert(create_empty_board() =:= {empty, empty, empty, empty, empty, empty, empty, empty, empty}).
+
+check_board_for_victory_in_row_test() ->
+	Board1 = {x, x, x,
+		 o, o, empty,
+		 empty, empty, empty},
+	?assert(check_board(Board1) =:= {victory, x}),
+
+	Board2 = {o, o, o,
+		  empty, x, empty,
+		  empty, empty, x},
+	?assert(check_board(Board2) =:= {victory, o}).
+
+check_board_for_victory_in_column_test() ->
+	Board1 = {x, o, o,
+		  x, empty, empty,
+		  x, empty, empty},
+	?assert(check_board(Board1) =:= {victory, x}).
+
+check_board_for_victory_in_diagonal_test() ->
+	Board1 = {o, x, empty,
+		  empty, o, x,
+		  empty,x, o},
+	?assert(check_board(Board1) =:= {victory, o}).
+
