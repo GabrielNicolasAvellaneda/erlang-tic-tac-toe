0fddcf03a26875fb4d9adf5385644155f97923f7
GabrielNicolasAvellaneda <avellaneda.gabriel@gmail.com>
2015-11-18 12:19:06 -0300

    tictactoe server process skeleton.
    Signed-off-by: GabrielNicolasAvellaneda <avellaneda.gabriel@gmail.com>

---
+++ tictactoe.erl
@@ -0,0 +1,20 @@
+-module(tictactoe).
+-export([start/0, init/0]).
+
+-include_lib("eunit/include/eunit.hrl").
+
+-define(TICTACTOE_INSTANCE_NAME, tictactoe_server).
+-define(TICTACTOE_NODE_NAME, 'server@vagrant-ubuntu-trusty-64').
+
+%% NOTE: You must run the tictactoe server process in a node named as defined by TICTACTOE_NODE_NAME.
+start() ->
+	Pid = spawn(?MODULE, init, []),
+	register(?TICTACTOE_INSTANCE_NAME, Pid). 
+
+init() ->
+	io:format("init()~n", []),
+	loop().
+
+loop() ->
+	loop().
+
